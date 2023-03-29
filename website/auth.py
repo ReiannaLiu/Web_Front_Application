816616d35ad20f72e48f197f6d451e2bc60e52bc
ReiannaLiu <rl3176@barnard.edu>
2023-03-29 16:12:18 -0400

    Connection to database

--- website/auth.py
+++ website/auth.py
@@ -30,6 +30,6 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            flash('Account created!', category='successß')
+            flash('Account created!', category='success')
 
     return render_template("sign_up.html")
