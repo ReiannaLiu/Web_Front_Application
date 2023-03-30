214cbd76e75fd2803cc54379d8e9142881b9f62f
ReiannaLiu <rl3176@barnard.edu>
2023-03-30 14:12:58 -0400

    search commit

--- website/views.py
+++ website/views.py
@@ -1,23 +1,30 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from website import DATABASEURI
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask_login import login_user, login_required, logout_user, current_user
+url_for, request
 
 views = Blueprint('views', __name__)
+
+engine = create_engine(DATABASEURI)
 
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        search = request.form.get('search')
+
+        with engine.connect() as conn:
+            params = {}
+            params["search"] = search
+
+            cursor = conn.execute(
+                text("SELECT name, color, size, description FROM product P LIMIT 5")
+            )
+
+            rowResults = cursor.fetchall()
+        return render_template("search.html", recentRecords=rowResults)
+
     return render_template("home.html")
-
-
-@views.route('/login')
-def login():
-    return render_template("login.html")
-
-
-@views.route('/logout')
-def logout():
-    return redirect(url_for('auth.login'))
-
-
-@views.route('/sign-up')
-def sign_up():
-    return render_template("sign_up.html")
