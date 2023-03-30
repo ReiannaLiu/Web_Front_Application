7ee4cb06e72caded08f4a0fd161638d65e09d56c
ReiannaLiu <rl3176@barnard.edu>
2023-03-30 09:59:45 -0400

    search

--- website/views.py
+++ website/views.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, redirect, url_for
 views = Blueprint('views', __name__)
 
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("home.html")
 
